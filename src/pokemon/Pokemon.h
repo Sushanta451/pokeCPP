d46a9e034accb4152ca2686a3664c297daccf925
sushanta <sushanta.sapkota10@gmail.com>
2026-01-06 20:54:46 -0500

    .

---
+++ src/pokemon/Pokemon.h
@@ -0,0 +1,57 @@
+#ifndef POKEMON_H
+#define POKEMON_H
+#include <string>
+
+class Pokemon {
+private:
+    // basic info of a pokemon
+    std::string name;
+    int level;
+    int exp;
+
+    // stats
+    int maxhp;
+    int hp;
+    int attack;
+    int defense;
+    int spa;
+    int spd;
+    int speed;
+
+    // ivs
+    int hp_iv;
+    int attack_iv;
+    int defense_iv;
+    int spa_iv;
+    int spd_iv;
+    int speed_iv;
+
+    // evs - 
+    int hp_ev;
+    int attack_ev;
+    int defense_ev;
+    int spa_ev;
+    int spd_ev;
+    int speed_ev;
+
+    // base stat
+    int hp_base;
+    int attack_base;
+    int defense_base;
+    int spa_base;
+    int spd_base;
+    int speed_base;
+
+    void calc_stats();
+    int generate_iv();
+
+public:
+    // Constructor
+    Pokemon(std::string name, int level) {
+        this->name = name;
+        this->level = level;
+    }
+
+};
+
+#endif
